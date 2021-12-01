6c20758949c5e1792cf96d4d661cdda2f5e802f9
Brandon Sturgeon <brandon@brandonsturgeon.com>
2021-12-01 15:50:34 -0800

    Adjustments

--- lua/entities/gmod_wire_expression2/core/custom/playercore.lua
+++ lua/entities/gmod_wire_expression2/core/custom/playercore.lua
@@ -4,16 +4,15 @@ E2Lib.RegisterExtension("playercore", true)
 local sbox_E2_PlyCore = CreateConVar("sbox_E2_PlyCore", "2", FCVAR_ARCHIVE)
 
 local function ValidPly(ply)
-	if not IsValid(ply) or not ply:IsPlayer() then
-		return false
-	end
+    if not IsValid( ply ) then return false end
+    if not ply:IsPlayer() then return false end
 
 	return true
 end
 
 
 local function hasAccess(ply, target, command)
-	local valid = hook.Call("PlyCoreCommand", GAMEMODE, ply, target, command)
+	local valid = hook.Run("PlyCoreCommand", ply, target, command)
 
 	if valid ~= nil then
 		return valid
@@ -321,75 +320,28 @@ e2function void entity:plyExtinguish()
 	this:Extinguish()
 end
 
-e2function string entity:ip()
-	if not ValidPly(this) or this:IsBot() then return "" end
-	local valid = hook.Call("PlyCoreCommand", GAMEMODE, self.player, nil, "getip")
-
-	if valid == nil then
-		valid = self.player:IsAdmin()
-	end
-
-	if valid then
-		return this:IPAddress()
-	end
-
-	return ""
-end
-
 -- Message
 
 e2function void sendMessage(string text)
-	if not hasAccess(self.player, nil, "globalmessage") then return nil end
-
-	PrintMessage(HUD_PRINTCONSOLE, self.player:Name() .. " send you the next message by an expression 2.")
-	PrintMessage(HUD_PRINTTALK, text)
 end
 
 e2function void sendMessageCenter(string text)
-	if not hasAccess(self.player, nil, "globalmessagecenter") then return nil end
-
-	PrintMessage(HUD_PRINTCONSOLE, text)
-	PrintMessage(HUD_PRINTCENTER, text)
 end
 
 --
 
 e2function void entity:sendMessage(string text)
-	if not ValidPly(this) then return nil end
-	if not hasAccess(self.player, this, "message") then return nil end
-
-	this:PrintMessage(HUD_PRINTCONSOLE, self.player:Name() .. " send you the next message by an expression 2.")
-	this:PrintMessage(HUD_PRINTTALK, text)
 end
 
 e2function void entity:sendMessageCenter(string text)
-	if not ValidPly(this) then return nil end
-	if not hasAccess(self.player, this, "messagecenter") then return nil end
-
-	this:PrintMessage(HUD_PRINTCONSOLE, self.player:Name() .. " send you the next message by an expression 2.")
-	this:PrintMessage(HUD_PRINTCENTER, text)
 end
 
 --
 
 e2function void array:sendMessage(string text)
-	for _, ply in pairs(this) do
-		if not ValidPly(ply) then return nil end
-		if not hasAccess(self.player, ply, "message") then return nil end
-
-		ply:PrintMessage(HUD_PRINTCONSOLE, self.player:Name() .. " send you the next message by an expression 2.")
-		ply:PrintMessage(HUD_PRINTTALK, text)
-	end
 end
 
 e2function void array:sendMessageCenter(string text)
-	for _, ply in pairs(this) do
-		if not ValidPly(ply) then return nil end
-		if not hasAccess(self.player, ply, "messagecenter") then return nil end
-
-		ply:PrintMessage(HUD_PRINTCONSOLE, self.player:Name() .. " send you the next message by an expression 2.")
-		ply:PrintMessage(HUD_PRINTCENTER, text)
-	end
 end
 
 
@@ -480,50 +432,18 @@ e2function void sendMessageColor(array arr)
 end
 
 e2function void sendMessageColor(...)
-	-- if not ValidPly(this) then return end
-	if not hasAccess(self.player, nil, "globalmessagecolor") then return nil end
-
-	printColorVarArg(self.player, player.GetAll(), typeids, ...)
 end
 
 e2function void entity:sendMessageColor(array arr)
-	if not ValidPly(this) then return end
-	if not hasAccess(self.player, this, "messagecolor") then return nil end
-
-	printColorArray(self.player, this, arr)
 end
 
 e2function void entity:sendMessageColor(...)
-	if not ValidPly(this) then return end
-	if not hasAccess(self.player, this, "messagecolor") then return nil end
-
-	printColorVarArg(self.player, this, typeids, ...)
 end
 
 e2function void array:sendMessageColor(array arr)
-	local plys = {}
-
-	for _, ply in pairs(this) do
-		if not ValidPly(ply) then continue end
-		if not hasAccess(self.player, ply, "messagecolor") then continue end
-
-		table.insert(plys, ply)
-	end
-
-	printColorArray(self.player, plys, arr)
 end
 
 e2function void array:sendMessageColor(...)
-	local plys = {}
-
-	for _, ply in pairs(this) do
-		if not ValidPly(ply) then continue end
-		if not hasAccess(self.player, ply, "messagecolor") then continue end
-
-		table.insert(plys, ply)
-	end
-
-	printColorVarArg(self.player, plys, typeids, ...)
 end
 
 
@@ -537,18 +457,18 @@ registerCallback("destruct",function(self)
 		registered_e2s_spawn[self.entity] = nil
 end)
 
-hook.Add("PlayerSpawn","Expresion2_PlayerSpawn", function(ply)
-	local ents = {}
-
-	for entity,_ in pairs(registered_e2s_spawn) do
-		if entity:IsValid() then table.insert(ents, entity) end
-	end
-
+hook.Add("PlayerSpawn","PlyCore_PlayerSpawn", function(ply)
 	respawnrun = 1
 	lastspawnedplayer = ply
-	for _,entity in ipairs(ents) do
-		entity:Execute()
+
+	for e2 in pairs(registered_e2s_spawn) do
+		if IsValid( e2:IsValid() ) then
+		    e2:Execute()
+        else
+            registered_e2s_spawn[e2] = nil
+        end
 	end
+
 	respawnrun = 0
 end)
 
@@ -578,18 +498,18 @@ registerCallback("destruct",function(self)
 		registered_e2s_death[self.entity] = nil
 end)
 
-hook.Add("PlayerDeath","Expresion2_PlayerDeath", function(victim, inflictor, attacker)
-	local ents = {}
-
-	for entity,_ in pairs(registered_e2s_death) do
-		if entity:IsValid() then table.insert(ents, entity) end
-	end
-
+hook.Add("PlayerDeath", "PlyCore_PlayerDeath", function(victim, inflictor, attacker)
 	deathrun = 1
 	playerdeathinfo = { victim, inflictor, attacker}
-	for _,entity in ipairs(ents) do
-		entity:Execute()
+
+	for e2 in pairs(registered_e2s_death) do
+		if IsValid( e2 ) then
+		    entity:Execute()
+        else
+            registered_e2s_death[e2] = nil
+        end
 	end
+
 	deathrun = 0
 end)
 
@@ -627,14 +547,16 @@ registerCallback("destruct",function(self)
 		registered_e2s_connect[self.entity] = nil
 end)
 
-hook.Add("PlayerInitialSpawn","Expresion2_PlayerInitialSpawn", function(ply)
+hook.Add("PlayerInitialSpawn","PlyCore_PlayerInitialSpawn", function(ply)
 	connectrun = 1
 	lastconnectedplayer = ply
 
-	for entity,_ in pairs(registered_e2s_connect) do
-		if entity:IsValid() then
+	for e2 in pairs(registered_e2s_connect) do
+		if IsValid( e2 ) then
 			entity:Execute()
-		end
+        else
+            registered_e2s_connect[e2] = nil
+        end
 	end
 
 	connectrun = 0
@@ -666,14 +588,16 @@ registerCallback("destruct",function(self)
 		registered_e2s_disconnect[self.entity] = nil
 end)
 
-hook.Add("PlayerDisconnected","Expresion2_PlayerDisconnected", function(ply)
+hook.Add("PlayerDisconnected","PlyCore_PlayerDisconnected", function(ply)
 	disconnectrun = 1
 	lastdisconnectedplayer = ply
 
-	for entity,_ in pairs(registered_e2s_disconnect) do
-		if entity:IsValid() then
+	for e2 in pairs(registered_e2s_disconnect) do
+		if IsValid( e2 ) then
 			entity:Execute()
-		end
+        else
+            registered_e2s_disconnect[e2] = nil
+        end
 	end
 
 	disconnectrun = 0
